4e16e8489784933647731bb322a0378f2c8139fe
jalT <laughtrack55@protonmail.com>
2025-05-02 20:43:33 -0700

    folder

--- pognConfigs.js
+++ pognConfigs.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const ENV = process.env.ENV || "development";
 console.log(`🚀 Starting POGN in ${ENV} mode...`);
-const PERMISSIONS = require("./permissionConfigs");
+const PERMISSIONS = require("./casinoForge/ghUtils/permissionConfigs");
 const INITGAMES_CONFIG = require("./initGamesConfigs");
 const INITGAMES = INITGAMES_CONFIG[ENV] || [];
 const CONFIGS = {
